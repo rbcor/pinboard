6af81175707a8f22368e012f7d4954bf099395c6
Rabiul Islam <rb60041@gmail.com>
2017-09-06 21:59:48 +0600

    allow all host

--- pinboardpro/settings.py
+++ pinboardpro/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = os.environ.get('DJANGO_SECRET_KEY', 'n5abe%w5eyubp1qr@wfj2x-!f(_$^a
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = bool(os.environ.get('DIBUG_STATUS', True))
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
